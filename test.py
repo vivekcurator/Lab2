1e67573d751d90118d0b9b8193b6abc135760ceb
vivekcurator <sainivivek62@gmail.com>
2021-01-26 21:27:25 +0530

    First Commit

---
+++ test.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 22 21:43:21 2020
+
+@author: viveksaini
+"""
+from PIL import Image
+
+a = ("67","hello",3,26,"kiwi","m","python")
+a = a[1:3]
+print(a)
