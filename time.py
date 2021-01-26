686384a4c21861f3cc2d4288e1d9e33b9591c723
vivekcurator <sainivivek62@gmail.com>
2021-01-26 21:30:11 +0530

    first commit

---
+++ time.py
@@ -0,0 +1,10 @@
+from datetime import datetime
+
+# datetime object containing current date and time
+now = datetime.now()
+ 
+#print("now =", now)
+
+# dd/mm/YY H:M:S
+dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
+print("DT =", dt_string)
